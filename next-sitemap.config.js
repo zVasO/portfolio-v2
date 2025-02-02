924555525f33239dd4a31774e7b648835440d488
Dylan Germann <dev.dyger@gmail.com>
2025-02-02 10:22:10 +0100

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    siteUrl: 'https://www.dgermann.dev/', // Remplace par l'URL de ton site
+    generateRobotsTxt: true, // Facultatif : génère un fichier robots.txt
+    changefreq: 'daily', // Fréquence de mise à jour de tes pages
+    priority: 0.7, // Priorité générale de ton site
+  };
